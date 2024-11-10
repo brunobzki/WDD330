ee5dc550535bd3639d422508ed5384d654cec785
brunobzki <bpanzacchi@gmail.com>
2024-11-09 22:16:03 -0300

    Add week 01

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,26 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+
+  let cart = getLocalStorage("so-cart") || [];
+
+    if (!Array.isArray(cart)) {
+      cart = [];
+    } 
+
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
+  console.log(JSON.parse(localStorage.getItem("so-cart")));
+
   addProductToCart(product);
 }
-
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
