f880819ddae6c53c3ccbb4b28451d77aabb973c9
brunobzki <bpanzacchi@gmail.com>
2024-11-21 21:04:37 -0300

    week 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,12 @@
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { setLocalStorage, getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
-const product = new ProductDetails(productId, dataSource)
+const productId = getParam("product");
 
-
-product.init()
+const product = new ProductDetails(productId, dataSource);
+product.init();
 /*
 function addProductToCart(product) {
 
