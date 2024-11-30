294b25da291ae27d8ecc7a3afea6253379f48346
Axel Herrera <axelh260498@gmail.com>
2024-11-30 09:45:21 -0300

    undefined cart, vite config y demas solucionado PAPA!

--- src/js/product.js
+++ src/js/product.js
@@ -1,32 +1,10 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ExternalServices from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { getParam, loadHeaderFooter, setLocalStorage, getLocalStorage } from "/js/utils.mjs";
+import ExternalServices from "/js/ProductData.mjs";
+import ProductDetails from "/js/ProductDetails.mjs";
 
 loadHeaderFooter();
-// <<<<<<< brunow2--individual
 const dataSource = new ExternalServices("tents");
 const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-    if (!Array.isArray(cart)) {
-      cart = [];
-    } 
-
-  cart.push(product);
-
-  setLocalStorage("so-cart", cart);
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  console.log(JSON.parse(localStorage.getItem("so-cart")));
-
-  addProductToCart(product);
-}
-
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
