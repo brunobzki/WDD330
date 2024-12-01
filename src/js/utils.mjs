d8216ea846120449a33b7397c2b32798da122241
brunobzki <bpanzacchi@gmail.com>
2024-12-01 01:52:28 -0300

    ads

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -60,7 +60,7 @@ async function loadTemplate(path) {
 export async function loadHeaderFooter() {
   const headerTemplate = await loadTemplate("/public/partials/header.html");
   const headerElement = document.querySelector("#main-header");
-  const footerTemplate = await loadTemplate("public/partials/footer.html");
+  const footerTemplate = await loadTemplate("/public/partials/footer.html");
   const footerElement = document.querySelector("#main-footer");
 
   renderWithTemplate(headerTemplate, headerElement);
