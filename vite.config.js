7034aa1705c543e570e37498b4d9739dbf197c89
Alan Gonzalez <alan.gonzalez87@hotmail.com>
2024-12-04 11:32:43 -0300

    fir error url relativas

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,27 @@
-import { resolve } from "path";
+// import { resolve } from "path";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   root: "src/",
+
+//   build: {
+//     outDir: "../dist",
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, "src/index.html"),
+//         cart: resolve(__dirname, "src/cart/index.html"),
+//         product: resolve(__dirname, "src/checkout/index.html")
+//       },
+//     },
+//   },
+// });
+
+import { resolve, dirname } from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url"; // Necesario para usar __dirname en ESM
+
+// Simular __dirname en ESM
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: "src/",
@@ -10,8 +32,12 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
-        product: resolve(__dirname, "src/checkout/index.html")
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        checkedout: resolve(__dirname, "src/checkout/success.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        listing: resolve(__dirname, "src/product-listing/index.html"),
       },
     },
   },
 });
+
